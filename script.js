afbd5369cd0b19354e9f5669b68705976b1daa0b
KartiKBriteinno <153173760+KartiKBriteinno@users.noreply.github.com>
2024-04-20 22:45:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length < 2 || new Set(arr).size === 1) {
+        return -Infinity;
+    }
+    
+    let max = Math.max(...arr);
+    let secondMax = -Infinity;
+    
+    for (let num of arr) {
+        if (num !== max && num > secondMax) {
+            secondMax = num;
+        }
+    }
+    
+    return secondMax;
+}
